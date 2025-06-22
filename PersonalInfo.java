5aefdc408622e0a1c9473448446a143477de2439
DylanR19 <dylan.romero@csuglobal.edu>
2025-06-21 21:08:39 -0500

    Add files via upload

---
+++ PersonalInfo.java
@@ -0,0 +1,16 @@
+package myapp;
+public class PersonalInfo {
+	public static void main(String[] args) {
+		String firstName = "Kyle";
+		String lastName = "Smith";
+		String streetAddress = "123 Forest Road";
+		String city = "Tokyo";
+		String zipcode = "100-0001";
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipcode);
+	}
+}
